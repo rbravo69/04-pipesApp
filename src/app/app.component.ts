cdd375b992a7a365f3af59186f8d8a9c819dbbf4
Rafael Bravo <rbravo69@hotmail.com>
2022-06-21 14:09:01 -0500

    pipes y primeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+ 
+  nombre:string='rafael bravo'
+  valor:number=1000;
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule,
+    SharedModule,
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
