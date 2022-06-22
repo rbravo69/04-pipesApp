f9b9c797f644fbd9a27c053b93fa5b76d44fd7f6
Rafael Bravo <rbravo69@hotmail.com>
2022-06-21 23:21:10 -0500

    agregar basicos pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
-
-
+//cambiar el locale a español
+import  localeEs  from "@angular/common/locales/es-MX";
+import { registerLocaleData } from "@angular/common";
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { VentasModule } from './ventas/ventas.module';
     SharedModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    {provide:LOCALE_ID, useValue:'es-MX'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
